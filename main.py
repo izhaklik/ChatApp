20012afb5834b24e43afb22858f06df14b03042c
izhaklik <38430519+izhaklik@users.noreply.github.com>
2022-01-04 21:45:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,238 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+import socket_client
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+import sys
+
+
+kivy.require("1.10.1")
+
+
+class ConnectPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols = 2
+
+        with open("prev_details.txt","r") as f:
+            d = f.read().split(",")
+            prev_ip = d[0]
+            prev_port = d[1]
+            prev_username = d[2]
+
+        self.add_widget(Label(text='IP:'))  # widget #1, top left
+        self.ip = TextInput(text=prev_ip, multiline=False)  # defining self.ip...
+        self.add_widget(self.ip) # widget #2, top right
+
+        self.add_widget(Label(text='Port:'))
+        self.port = TextInput(text=prev_port, multiline=False)
+        self.add_widget(self.port)
+
+        self.add_widget(Label(text='Username:'))
+        self.username = TextInput(text=prev_username, multiline=False)
+        self.add_widget(self.username)
+
+        # add button.
+        self.join = Button(text="Join")
+        self.join.bind(on_press=self.join_button)
+        self.add_widget(Label())
+        self.add_widget(self.join)
+
+    def join_button(self, instance):
+        port = self.port.text
+        ip = self.ip.text
+        username = self.username.text
+        with open("prev_details.txt","w") as f:
+            f.write(f"{ip},{port},{username}")
+        #print(f"Joining {ip}:{port} as {username}")
+        # Create info string, update InfoPage with a message and show it
+        info = f"Joining {ip}:{port} as {username}"
+        chat_app.info_page.update_info(info)
+        chat_app.screen_manager.current = 'Info'
+        Clock.schedule_once(self.connect, 1)
+
+    # Connects to the server
+
+    def connect(self, _):
+
+        # Get information for sockets client
+        port = int(self.port.text)
+        ip = self.ip.text
+        username = self.username.text
+
+        if not socket_client.connect(ip, port, username, show_error):
+            return
+
+        # Create chat page and activate it
+        chat_app.create_chat_page()
+        chat_app.screen_manager.current = 'Chat'
+
+
+class ScrollableLabel(ScrollView):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.layout = GridLayout(cols=1, size_hint_y=None)
+        self.add_widget(self.layout)
+
+
+        self.chat_history = Label(size_hint_y=None, markup=True)
+        self.scroll_to_point = Label()
+
+
+        self.layout.add_widget(self.chat_history)
+        self.layout.add_widget(self.scroll_to_point)
+
+    def update_chat_history(self, message):
+
+        # First add new line and message itself
+        self.chat_history.text += '\n' + message
+
+        # Set layout height to whatever height of chat history text is + 15 pixels
+        # (adds a bit of space at teh bottom)
+        # Set chat history label to whatever height of chat history text is
+        # Set width of chat history text to 98 of the label width (adds small margins)
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width * 0.98, None)
+
+        # As we are updating above, text height, so also label and layout height are going to be bigger
+        # than the area we have for this widget. ScrollView is going to add a scroll, but won't
+        # scroll to the botton, nor there is a method that can do that.
+        # That's why we want additional, empty wodget below whole text - just to be able to scroll to it,
+        # so scroll to the bottom of the layout
+        self.scroll_to(self.scroll_to_point)
+
+
+
+class ChatPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols = 1
+        self.rows = 2
+
+        # First row is going to be occupied by our scrollable label
+        # We want it be take 90% of app height
+        self.history = ScrollableLabel(height=Window.size[1]*0.9, size_hint_y=None)
+        self.add_widget(self.history)
+
+        self.new_message = TextInput(width=Window.size[0]*0.8, size_hint_x=None, multiline=False)
+        self.send = Button(text="Send")
+        self.send.bind(on_press=self.send_message)
+
+
+        bottom_line = GridLayout(cols=2)
+        bottom_line.add_widget(self.new_message)
+        bottom_line.add_widget(self.send)
+        self.add_widget(bottom_line)
+
+
+
+        # To be able to send message on Enter key, its need to listen to keypresses
+        Window.bind(on_key_down=self.on_key_down)
+
+        Clock.schedule_once(self.focus_text_input, 1)
+
+
+        socket_client.start_listening(self.incoming_message, show_error)
+
+
+    # Gets called on key press
+    def on_key_down(self, instance, keyboard, keycode, text, modifiers):
+
+        # But we want to take an action only when Enter key is being pressed, and send a message
+        if keycode == 40:
+            self.send_message(None)
+
+    # Gets called when either Send button or Enter key is being pressed
+    def send_message(self, _):
+
+        # Get message text and clear message input field
+        message = self.new_message.text
+        self.new_message.text = ''
+
+        if message:
+            # Our messages - use red color for the name
+            self.history.update_chat_history(f'[color=dd2020]{chat_app.connect_page.username.text}[/color] > {message}')
+            socket_client.send(message)
+
+        Clock.schedule_once(self.focus_text_input, 0.1)
+
+
+    def focus_text_input(self, _):
+        self.new_message.focus = True
+
+    # Passed to sockets client, get's called on new message
+    def incoming_message(self, username, message):
+        # Update chat history with username and message, green color for username
+        self.history.update_chat_history(f'[color=20dd20]{username}[/color] > {message}')
+
+
+# Simple information/error page
+class InfoPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols = 1
+
+        self.message = Label(halign="center", valign="middle", font_size=30)
+
+
+        self.message.bind(width=self.update_text_width)
+
+        # Add text widget to the layout
+        self.add_widget(self.message)
+
+    # Called with a message, to update message text in widget
+    def update_info(self, message):
+        self.message.text = message
+
+    # Called on label width update, so we can set text width properly - to 90% of label width
+    def update_text_width(self, *_):
+        self.message.text_size = (self.message.width * 0.9, None)
+
+
+class EpicApp(App):
+    def build(self):
+
+
+        self.screen_manager = ScreenManager()
+
+        self.connect_page = ConnectPage()
+        screen = Screen(name='Connect')
+        screen.add_widget(self.connect_page)
+        self.screen_manager.add_widget(screen)
+
+        # Info page
+        self.info_page = InfoPage()
+        screen = Screen(name='Info')
+        screen.add_widget(self.info_page)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
+
+
+    def create_chat_page(self):
+        self.chat_page = ChatPage()
+        screen = Screen(name='Chat')
+        screen.add_widget(self.chat_page)
+        self.screen_manager.add_widget(screen)
+
+
+def show_error(message):
+    chat_app.info_page.update_info(message)
+    chat_app.screen_manager.current = 'Info'
+    Clock.schedule_once(sys.exit, 10)
+
+if __name__ == "__main__":
+    chat_app = EpicApp()
+    chat_app.run()
